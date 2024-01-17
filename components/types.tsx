5b2ee43d01a6f7e8942bae5e079e75ceab28c622
a-perkin <aleksandr.perkin90@gmail.com>
2024-01-17 21:49:14 +0200

    less1_done

---
+++ components/types.tsx
@@ -0,0 +1,10 @@
+import { Image } from "react-native";
+
+export type Item = {
+  title: string;
+  isNew: boolean;
+  image: any;
+  newPrice: string;
+  oldPrice: string;
+  description: string;
+};
